e5accf429ebe9e3ef11a62773c7806d9ec67b315
Abdelrahman Taha <abdelrahmantaha020@gmail.com>
2024-10-05 05:38:00 +0300

    added font family to our App

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 abstract class Styles {
   static const titleMeduim =
-      TextStyle(fontSize: 20, fontWeight: FontWeight.w600);
+    TextStyle(fontSize: 20, fontWeight: FontWeight.w600);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:bookly/constants.dart';
 import 'package:bookly/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,11 +16,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: TextTheme(
-          headlineLarge: TextStyle(fontSize: 80)
-        )
-      ),
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme:
+              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme)),
       debugShowCheckedModeBanner: false,
       home: const SplashView(),
     );
