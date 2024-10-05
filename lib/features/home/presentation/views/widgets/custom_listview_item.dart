0778796a47513124e19e2c91820ed4170295a420
Abdelrahman Taha <abdelrahmantaha020@gmail.com>
2024-10-05 05:09:57 +0300

    add custom Listview Item to Home View

---
+++ lib/features/home/presentation/views/widgets/custom_listview_item.dart
@@ -0,0 +1,28 @@
+
+import 'package:bookly/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class CustomListViewItem extends StatelessWidget {
+  const CustomListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).height * 0.18,
+      child: AspectRatio(
+        aspectRatio: 2.7 / 4,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: Colors.red,
+            image: const DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage(
+                  AssetsData.testImage,
+                )),
+          ),
+        ),
+      ),
+    );
+  }
+}
