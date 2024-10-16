b7ea93ef93662104e1c47f43d380cc438704e512
Abdelrahman Taha <abdelrahmantaha020@gmail.com>
2024-10-16 03:57:58 +0300

    add CustomScrollView

--- lib/features/home/presentation/views/widgets/book_details_viewbody.dart
+++ lib/features/home/presentation/views/widgets/book_details_viewbody.dart
@@ -12,63 +12,76 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30.0),
-      child: Column(
-        children: [
-          const CustomBookDetailsAppBar(),
-          const SizedBox(
-            height: 30,
-          ),
-          Padding(
-              padding: EdgeInsets.symmetric(horizontal: width * .2),
-              child: const CustomBookImage()),
-          const SizedBox(
-            height: 43,
-          ),
-          Text(
-            "The Jungle Book",
-            style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(
-            height: 6,
-          ),
-          Opacity(
-            opacity: 0.7,
-            child: Text(
-              "Rudyard kipling",
-              style: Styles.textStyle20.copyWith(
-                  color: Colors.grey,
-                  fontStyle: FontStyle.italic,
-                  fontWeight: FontWeight.w500),
-            ),
-          ),
-          const SizedBox(
-            height: 18,
-          ),
-          const BookRating(
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
-          const SizedBox(
-            height: 37,
-          ),
-          const BooksAction(),
-          const SizedBox(
-            height: 50,
-          ),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              "You can also like",
-              style: Styles.textStyle16.copyWith(fontWeight: FontWeight.w900),
-            ),
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          const SimiarBooksListview(),
-        ],
-      ),
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+            hasScrollBody: false,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30.0),
+              child: Column(
+                children: [
+                  const CustomBookDetailsAppBar(),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Padding(
+                      padding: EdgeInsets.symmetric(horizontal: width * .2),
+                      child: const CustomBookImage()),
+                  const SizedBox(
+                    height: 43,
+                  ),
+                  Text(
+                    "The Jungle Book",
+                    style: Styles.textStyle30
+                        .copyWith(fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 6,
+                  ),
+                  Opacity(
+                    opacity: 0.7,
+                    child: Text(
+                      "Rudyard kipling",
+                      style: Styles.textStyle20.copyWith(
+                          color: Colors.grey,
+                          fontStyle: FontStyle.italic,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 18,
+                  ),
+                  const BookRating(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                  ),
+                  const SizedBox(
+                    height: 37,
+                  ),
+                  const BooksAction(),
+                  const Expanded(
+                    child: SizedBox(
+                      height: 50,
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "You can also like",
+                      style: Styles.textStyle16
+                          .copyWith(fontWeight: FontWeight.w900),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  const SimiarBooksListview(),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                ],
+              ),
+            )),
+      ],
     );
   }
 }
