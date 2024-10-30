9b6a6cb3a4dbc674e8febca74c18bdf577701d8e
Abdelrahman Taha <abdelrahmantaha020@gmail.com>
2024-10-31 02:58:25 +0300

    search view done

--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,8 +1,10 @@
 import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/styles.dart';
+import 'package:bookly/features/home/presentation/manager/featured_books/featured_books_cubit.dart';
 import 'package:bookly/features/home/presentation/views/widgets/best_seller_listview_item.dart';
 import 'package:bookly/features/search/presentation/views/widgets/custom_search_textfield.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
@@ -40,15 +42,29 @@ class SearchResultsListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      // physics: const NeverScrollableScrollPhysics(),
-      padding: EdgeInsets.zero,
-      itemCount: 10,
-      itemBuilder: (BuildContext context, int index) {
-        return const Padding(
-          padding: EdgeInsets.symmetric(vertical: 10),
-          // child: BookListviewItem(booksModel: ,),
-        );
+    return BlocBuilder<FeaturedBooksCubit, FeaturedBooksState>(
+      builder: (context, state) {
+        if (state is FeaturedBooksSuccess) {
+          return ListView.builder(
+            physics: const BouncingScrollPhysics(),
+            padding: EdgeInsets.zero,
+            itemCount: state.books.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                child: BookListviewItem(
+                  booksModel: state.books[index],
+                ),
+              );
+            },
+          );
+        } else if (state is FeaturedBooksFailure) {
+          return ErrorWidget(state.errMessage);
+        } else {
+          return const CircularProgressIndicator(
+            color: Colors.white,
+          );
+        }
       },
     );
   }
